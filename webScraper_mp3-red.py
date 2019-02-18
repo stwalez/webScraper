3911b95497523970d87b83b33f68c268b0b64ff9
stwalez <stwalez1@gmail.com>
2019-02-18 21:30:12 +0100

    Updated requests to use Session objects

--- webScraper_mp3-red.py
+++ webScraper_mp3-red.py
@@ -1,7 +1,3 @@
-# Webscraping script for mp3-red.co v1
-# Created with love on Python
-# Jan 1 2019
-
 import requests
 import urllib
 from bs4 import BeautifulSoup
@@ -12,60 +8,75 @@ def scrape_mp3 ():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
     user_search_text = input("Which Artist would you like to search for?")
     data = {'str':user_search_text}
-    r = requests.post('http://mp3-red.co/Search',headers=headers, params=data)
-    url = 'http://mp3-red.co'
-    soup = url_page_info(r)
-    soup_run(url,soup,headers)
+    try:
+        ses=requests.session()
+        ses.headers.update(headers)
+        r = ses.post('http://mp3-red.co/Search',params=data)
+        url = 'http://mp3-red.co'
 
+        #print(ses.cookies.get_dict())
+        #print(ses.headers)
+
+        soup = url_page_info(r)
+        soup_run(url,soup,ses)
+    except ConnectionError:
+       print("Error in connecting, check your internet connection!!!")
 
 def url_page_info(r):
     source_code = r
     plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
+    soup = BeautifulSoup(plain_text,"html.parser")
     return soup
 
-def soup_run(url,soup,headers):
+def soup_run(url,soup,ses):
     for get_a_song in soup.findAll('a',{'class':'track-title'}):
         song_name = get_a_song.string
         print(song_name)
         href = url + get_a_song.get('href')
         print(href)
-        get_single_item_data(href,url,headers)
-    get_next_page(soup, url,headers)
+        get_single_item_data(href,url,ses)
+    get_next_page(soup, url,ses)
 
-def get_next_page(soup,url,headers):
+def get_next_page(soup,url,ses):
     global page_number
     page_number += 1
     try:
         next_page = soup.find('a',text='Next page').get('href')
         href = url + next_page
         print("Page", page_number,":", href)
-        r = requests.get(href,headers=headers)
+        r = ses.get(href)
         soup_np = url_page_info(r)
-        soup_run(url,soup_np,headers)
+        soup_run(url,soup_np,ses)
     except:
         print("---------------------------\n End of Scraping for the Search Item")
 
-def get_single_item_data(item_url,url, headers):
-    source_code = requests.get(item_url,headers=headers)
-
-    cookies1 = source_code.headers['Set-Cookie']
-    cookies2 = cookies1.split(' ')
-    cookies3 = [w.replace(';', '') for w in [x for x in cookies2 if "=" in x]]
-    cookies4 = dict(item.split("=") for item in cookies3)
-
+def get_single_item_data(item_url,url,ses):
+    """
+	Get a single page links for specified search item
+    """
+    source_code = ses.get(item_url)
     plain_text = source_code.text
     #print(plain_text)
-    soup = BeautifulSoup(plain_text)
+    soup = BeautifulSoup(plain_text, "html.parser")
     get_the_song = soup.find('a', {'id': 'download_link'})
     song_link = url + get_the_song.get('href')
     print(song_link)
 
-    r = requests.get(song_link, headers=headers,cookies=cookies4)
+    r = ses.get(song_link)
     print(r.status_code)
-    print(r.headers)
+    
     filename = get_filename_from_cd(r.headers.get('content-disposition'))
+    print("downloading...", filename)
     open(filename, 'wb').write(r.content)
+    print("Successfully downloaded", filename)
+
+    ## Testing a single file..
+    # with open("burna.mp3", "wb") as code:
+    #     code.write(r.content)
+               #response = urllib.urlopen("http://mp3-red.co/stream/20781389/burna-boy-bad-boy-feat-korkormikor.mp3")
+    #mp3 = response.read()
+    #open("burn2", 'wb').write(r)
+
 
 def get_filename_from_cd(cd):
     """
@@ -78,8 +89,11 @@ def get_filename_from_cd(cd):
         return None
     return fname[0]
 
-def is_downloadable(url):
 
+def is_downloadable(url):
+    """
+	Not used
+    """
     h = requests.head(url, allow_redirects=True)
     header = h.headers
     content_type = header.get('content-type')
